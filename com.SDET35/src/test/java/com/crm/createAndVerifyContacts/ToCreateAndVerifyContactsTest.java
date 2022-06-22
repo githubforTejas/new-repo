06891eb2baf63e0562243fe08d4d9fc08df678f5
githubforTejas <105335742+githubforTejas@users.noreply.github.com>
2022-06-22 13:08:12 +0530

    Update ToCreateAndVerifyContactsTest.java

--- com.SDET35/src/test/java/com/crm/createAndVerifyContacts/ToCreateAndVerifyContactsTest.java
+++ com.SDET35/src/test/java/com/crm/createAndVerifyContacts/ToCreateAndVerifyContactsTest.java
@@ -28,7 +28,7 @@ public class ToCreateAndVerifyContactsTest extends BaseClass{
 	@Test(groups="SmokeTest",retryAnalyzer = com.crm.genericutility.RetryAnalyserImp.class)
 	public  void toCreateAndVerifyContactsTest() throws IOException, InterruptedException {
 
-	         //To get the random number(Moolangi sambar)
+	         //To get the random number
 		int randomNum = java.getRandomNumber();
 
 		//to get the data from excel
